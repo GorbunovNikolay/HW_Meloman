db3dd9e6f9a25dc80b8f96f3cee0998571a168f1
Gorbunov Nikolay <gor_bunk@mail.ru>
2021-11-27 18:57:57 +0300

    Добавлен исполняемый код задачи

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,39 +2,53 @@ import java.math.RoundingMode
 import java.text.DecimalFormat
 
 fun main() {
-    var Ilya =      User("Илья",    501.70,  false) //по умолчанию без скидок
-    var Nikolay =   User("Николай", 1507.38, false) //по умолчанию скидка 100 руб
-    var Olga =      User("Ольга",   20579.90,false) //по умолчанию скидка 5% от суммы
-    var Valeriy =   User("Валерий", 55678.58,true)  //по умолчанию 5%, + доп 1%
-    calculateTheDiscount(Ilya)
-    calculateTheDiscount(Nikolay)
-    calculateTheDiscount(Olga)
-    calculateTheDiscount(Valeriy)
+    var Ilya =      User("Илья",    false)  //покупатель
+    var Nikolay =   User("Николай", true)   //постоянный покупатель
+
+
+    var priceMusic = 100.00
+    calculateTheDiscount(Ilya, priceMusic)
+
+    priceMusic = 1005.00
+    calculateTheDiscount(Ilya, priceMusic)
+
+    priceMusic = 10050.00
+    calculateTheDiscount(Ilya, priceMusic)
+    calculateTheDiscount(Nikolay, priceMusic)
 }
 
-class User(val name: String, var purchaseAmount: Double, var regularCustomer: Boolean) {
+class User(val name: String, var regularCustomer: Boolean) {
 
 }
 
-fun calculateTheDiscount(ThisUser: User) {
+fun calculateTheDiscount(ThisUser: User, price: Double) {
+    val charRub = '\u20BD'
+    println("покупка - $price $charRub →")
+
     var discount = 0.00
 
-    if (ThisUser.purchaseAmount >= 1001 && ThisUser.purchaseAmount <= 10000) {
+    if (price >= 1001 && price <= 10000) {
         discount = 100.00
     }
-    if (ThisUser.purchaseAmount >= 10001) {
-        discount = (5 * ThisUser.purchaseAmount) / 100
+
+    if (price >= 10001) {
+        discount = (5 * price) / 100
+        var discount5percent = discount
+
+        println("после применения 5% скидки - " + (price - discount5percent))
+
         if (ThisUser.regularCustomer) {
-            var discount1percent = 1 * (ThisUser.purchaseAmount - discount) / 100
-            discount = discount + discount1percent
+            var discount1percent = 1 * (price - discount) / 100
+            discount = price - (discount + discount1percent)
+            println("после применения 1% скидки - $discount")
         }
     }
+
 
     var df = DecimalFormat("#.##")
     df.roundingMode = RoundingMode.CEILING
     var resultDiscount = df.format(discount)
-    val charRub = '\u20BD'
-
-    println("${ThisUser.name} Ваша сумма скидки составляет: $resultDiscount $charRub")
+    
+    println("${ThisUser.name} Ваша сумма скидки составляет: $resultDiscount $charRub" + "\n")
 }
 
