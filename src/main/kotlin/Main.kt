49dc9156501ad8ccd00d6311a07fc36970a2585b
Gorbunov Nikolay <gor_bunk@mail.ru>
2021-11-27 17:37:07 +0300

    Добавлено решение задачи

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,40 @@
+import java.math.RoundingMode
+import java.text.DecimalFormat
 
 fun main() {
-    println("Привет")
+    var Ilya =      User("Илья",    501.70,  false) //по умолчанию без скидок
+    var Nikolay =   User("Николай", 1507.38, false) //по умолчанию скидка 100 руб
+    var Olga =      User("Ольга",   20579.90,false) //по умолчанию скидка 5% от суммы
+    var Valeriy =   User("Валерий", 55678.58,true)  //по умолчанию 5%, + доп 1%
+    calculateTheDiscount(Ilya)
+    calculateTheDiscount(Nikolay)
+    calculateTheDiscount(Olga)
+    calculateTheDiscount(Valeriy)
 }
+
+class User(val name: String, var purchaseAmount: Double, var regularCustomer: Boolean) {
+
+}
+
+fun calculateTheDiscount(ThisUser: User) {
+    var discount = 0.00
+
+    if (ThisUser.purchaseAmount >= 1001 && ThisUser.purchaseAmount <= 10000) {
+        discount = 100.00
+    }
+    if (ThisUser.purchaseAmount >= 10001) {
+        discount = (5 * ThisUser.purchaseAmount) / 100
+        if (ThisUser.regularCustomer) {
+            var discount1percent = 1 * (ThisUser.purchaseAmount - discount) / 100
+            discount = discount + discount1percent
+        }
+    }
+
+    var df = DecimalFormat("#.##")
+    df.roundingMode = RoundingMode.CEILING
+    var resultDiscount = df.format(discount)
+    val charRub = '\u20BD'
+
+    println("${ThisUser.name} Ваша сумма скидки составляет: $resultDiscount $charRub")
+}
+
